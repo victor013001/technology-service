21a52f51b915553925fbd8709aab8638d500c8a9
victor013001 <victormanuelosoriog@gmail.com>
2025-05-19 12:17:55 -0500

    HU-2 feat: add integration test

---
+++ src/test/java/com/pragma/challenge/technology_service/TechnologyRouterRestIT.java
@@ -0,0 +1,84 @@
+package com.pragma.challenge.technology_service;
+
+import static com.pragma.challenge.technology_service.util.TechnologyDtoDataUtil.getInvalidTechnologyDto;
+import static com.pragma.challenge.technology_service.util.TechnologyDtoDataUtil.getTechnologyDto;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.pragma.challenge.technology_service.domain.enums.ServerResponses;
+import com.pragma.challenge.technology_service.domain.exceptions.StandardError;
+import com.pragma.challenge.technology_service.infrastructure.adapters.persistence.entity.TechnologyEntity;
+import com.pragma.challenge.technology_service.infrastructure.adapters.persistence.repository.TechnologyRepository;
+import com.pragma.challenge.technology_service.infrastructure.entrypoints.dto.DefaultServerResponse;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+@Import(TestcontainersConfiguration.class)
+public class TechnologyRouterRestIT {
+
+  private final String BASE_URL = "/api/v1/technology";
+
+  @Autowired WebTestClient webTestClient;
+  @Autowired TechnologyRepository technologyRepository;
+
+  @BeforeEach
+  void setUp() {
+    technologyRepository
+        .saveAll(
+            List.of(
+                TechnologyEntity.builder()
+                    .name("Node.js")
+                    .description("Open-source, cross-platform JavaScript runtime environment")
+                    .build(),
+                TechnologyEntity.builder()
+                    .name("React")
+                    .description(
+                        "JavaScript library used for building particularly single-page applications")
+                    .build()))
+        .blockLast();
+  }
+
+  @Test
+  void createTechnology() {
+    webTestClient
+        .post()
+        .uri(BASE_URL)
+        .bodyValue(getTechnologyDto())
+        .exchange()
+        .expectStatus()
+        .isCreated()
+        .expectBody(DefaultServerResponse.class)
+        .consumeWith(
+            exchangeResult -> {
+              var response = exchangeResult.getResponseBody();
+              assertNotNull(response);
+              assertEquals(ServerResponses.TECHNOLOGY_CREATED.getMessage(), response.data());
+            });
+  }
+
+  @Test
+  void createTechnologyBadRequest() {
+    webTestClient
+        .post()
+        .uri(BASE_URL)
+        .bodyValue(getInvalidTechnologyDto())
+        .exchange()
+        .expectStatus()
+        .isBadRequest()
+        .expectBody(StandardError.class)
+        .consumeWith(
+            exchangeResult -> {
+              var error = exchangeResult.getResponseBody();
+              assertNotNull(error);
+              assertEquals(ServerResponses.BAD_REQUEST.getMessage(), error.getDescription());
+            });
+  }
+}
